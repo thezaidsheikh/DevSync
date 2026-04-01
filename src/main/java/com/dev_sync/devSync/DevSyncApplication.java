ef521f5720b7d59fec348e7896d01b11f5e7877b
Zaid Sheikh <zaidsheikh530@gmail.com>
2026-04-01 15:21:39 +0530

    chore: Add a spring default server

---
+++ src/main/java/com/dev_sync/devSync/DevSyncApplication.java
@@ -0,0 +1,13 @@
+package com.dev_sync.devSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev_sync/devSync/DevSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_sync.devSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
